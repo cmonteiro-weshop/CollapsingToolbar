defb3de722c7c3bcba1d964870813dcce02daf7a
Carlos Monteiro <carlos.monteiro@we.shop>
2023-03-08 16:55:04 +0000

    app is working

--- app/src/main/java/com/example/collapsingtoolbar/MainActivity2.kt
+++ app/src/main/java/com/example/collapsingtoolbar/MainActivity2.kt
@@ -2,10 +2,27 @@ package com.example.collapsingtoolbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.example.collapsingtoolbar.databinding.ActivityMain2Binding
+import com.google.android.material.appbar.AppBarLayout
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main2)
+        coordinateMotion()
+    }
+
+    private fun coordinateMotion() {
+        val appBarLayout: AppBarLayout = findViewById(R.id.appbar_layout)
+        val motionLayout: MotionLayout = findViewById(R.id.motion_layout)
+
+        val listener = AppBarLayout.OnOffsetChangedListener { _, verticalOffset ->
+            val seekPosition = -verticalOffset / appBarLayout.totalScrollRange.toFloat()
+            motionLayout.progress = seekPosition
+        }
+
+        appBarLayout.addOnOffsetChangedListener(listener)
     }
 }
